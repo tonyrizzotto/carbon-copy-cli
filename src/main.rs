bdc7e0a0ebcb365225a30611047ec7117e268d8c
Tony Rizzotto <tony@tonyrizzotto.com>
2022-12-10 06:39:43 -0500

    chore: cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,21 @@ use clap::{Parser, Subcommand};
 struct CarbonCopyArgs {
    #[command(subcommand)]
     command: Option<Commands>,
-   // /// Number of times to greet
-   // #[arg(short, long, default_value_t = 1)]
-   // count: u8,
 }
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-   /// implements the copy feature
+   /// Copy a template
    Copy {
-      /// Directory of the file you want to copy
+      /// Directory of the file you want to 
       #[arg(short, long)]
-      file: Option<String>,
-   },
-
-   /// Traverse a folder path to a specific file
-   Find {
-      /// Directory of the file you want to copy
-      #[arg(short, long)]
-      file: Option<String>,
-   },
+      folder: Option<String>,
+   }
 }
 
 
 fn main() {
-   let args = CarbonCopyArgs::parse();
+   // let args = CarbonCopyArgs::parse();
 
    // Default message for no args
    println!("Howdy 👋, I'm smart, but not that smart. Give me something to do, friend!")
